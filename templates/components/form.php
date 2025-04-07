76cd8610055370ffd1e79d3c199faee880a779f6
Jensone <hello@jensone.com>
2025-04-07 16:18:54 +0200

    Amélioration du formulaire avec ajout de champs pour l'email et le téléphone, et refonte du style pour mieux guider l'utilisateur

--- templates/components/form.php
+++ templates/components/form.php
@@ -3,15 +3,17 @@ Mail(r) / Telephone
 Objet (Autre, formation, collaboration, Devis,)
 Message -->
 <div class="m-4 shadow-sm">
-    <form action="" method="post" class="">
+    <form action="" method="post" class="flex flex-col gap-3">
+        <!-- Objets -->
         <div class="border border-slate-200 rounded-lg px-4 py-2 focus:border-slate-400 focus:shadow-md focus:outline-none">
-            <select name="object" id="object">
+            <select name="object" id="object" class="bg-transparent text-slate-800 w-full">
                 <option value="Collaboration">Collaboration</option>
                 <option value="Formation">Formation</option>
                 <option value="Devis gratuit">Devis gratuit</option>
                 <option value="Autre">Autre...</option>
             </select>
         </div>
+        <!-- Identité -->
         <div class="flex gap-4 my-4">
             <div class="flex flex-col gap-2">
                 <label for="firstname" class="font-semibold text-lg">Votre prénom</label>
@@ -22,7 +24,19 @@ Message -->
                 <input type="text" name="lastname" id="lastname" placeholder="Dupont" value="Dupont" minlength="2" maxlength="80" class="border border-slate-200 rounded-lg px-4 py-2 focus:border-slate-400 focus:shadow-md focus:outline-none" required>
             </div>
         </div>
-
+        <!-- Contact -->
+        <div class="flex gap-4 my-4">
+            <div class="flex flex-col gap-2">
+                <label for="email" class="font-semibold text-lg">Adresse email</label>
+                <input type="email" name="email" id="firstname" placeholder="martin@gmail.com" value="martin@gmail.com" maxlength="80" class="border border-slate-200 rounded-lg px-4 py-2 focus:border-slate-400 focus:shadow-md focus:outline-none" required>
+            </div>
+            <div class="flex flex-col gap-2">
+                <label for="telephone" class="font-semibold text-lg">Téléphone</label>
+                <input type="tel" name="telephone" id="lastname" placeholder="0612345678" value="0612345678" minlength="10" maxlength="12" class="border border-slate-200 rounded-lg px-4 py-2 focus:border-slate-400 focus:shadow-md focus:outline-none" required>
+            </div>
+        </div>
+        <!-- Message -->
+        <textarea name="message" id="message" rows="10" class="border border-slate-200 rounded-lg px-4 py-2 focus:border-slate-400 focus:shadow-md focus:outline-none" placeholder="Votre message..." required minlength="100"></textarea>
         <button type="submit" class="bg-white text-slate-800 rounded-lg shadow-md p-2 mt-4 hover:text-white hover:bg-slate-800">
             Envoyer
         </button>
